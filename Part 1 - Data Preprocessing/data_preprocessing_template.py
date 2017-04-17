801d7a88bfa2ffc5cd325acdd652b0b9ead94f39
Darragh ORiordan <darragh.oriordan@gmail.com>
2017-04-17 21:23:21 +1200

    add missing data

--- Part 1 - Data Preprocessing/data_preprocessing_template.py
+++ Part 1 - Data Preprocessing/data_preprocessing_template.py
@@ -6,9 +6,17 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # Importing the dataset
+
 dataset = pd.read_csv('Data.csv')
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, 3].values
+
+#taking care of missing data
+# added for session data
+from sklearn.preprocessing import Imputer
+imputer = Imputer(missing_values = 'NaN', strategy = 'mean', axis = 0)
+imputer.fit(X[:, 1:3])
+X[:, 1:3] = imputer.transform(X[:, 1:3])
 
 # Splitting the dataset into the Training set and Test set
 from sklearn.cross_validation import train_test_split
