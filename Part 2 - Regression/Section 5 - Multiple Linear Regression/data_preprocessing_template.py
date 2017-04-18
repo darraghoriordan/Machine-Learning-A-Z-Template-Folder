997d1fb8a3be5d01f5e8cd8853269e8e98bf6182
Darragh ORiordan <darragh.oriordan@gmail.com>
2017-04-18 21:54:38 +1200

    encode the categorical data

--- Part 2 - Regression/Section 5 - Multiple Linear Regression/data_preprocessing_template.py
+++ Part 2 - Regression/Section 5 - Multiple Linear Regression/data_preprocessing_template.py
@@ -6,9 +6,17 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # Importing the dataset
-dataset = pd.read_csv('Data.csv')
+dataset = pd.read_csv('50_Startups.csv')
 X = dataset.iloc[:, :-1].values
-y = dataset.iloc[:, 3].values
+y = dataset.iloc[:, 4].values
+
+# Encoding categorical data
+# Encoding the Independent Variable
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder_X = LabelEncoder()
+X[:, 3] = labelencoder_X.fit_transform(X[:, 3])
+onehotencoder = OneHotEncoder(categorical_features = [3])
+X = onehotencoder.fit_transform(X).toarray()
 
 # Splitting the dataset into the Training set and Test set
 from sklearn.cross_validation import train_test_split
