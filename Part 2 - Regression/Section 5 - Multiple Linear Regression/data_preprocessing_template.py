d914f9fa60ce61bac0c09d5b3c1f18eb3d0234fb
Darragh ORiordan <darragh.oriordan@gmail.com>
2017-04-18 22:05:08 +1200

    predict the test set

--- Part 2 - Regression/Section 5 - Multiple Linear Regression/data_preprocessing_template.py
+++ Part 2 - Regression/Section 5 - Multiple Linear Regression/data_preprocessing_template.py
@@ -37,3 +37,7 @@ y_train = sc_y.fit_transform(y_train)"""
 from sklearn.linear_model import LinearRegression
 regressor = LinearRegression()
 regressor.fit(X_train, y_train)
+
+# predict the test set
+y_pred = regressor.predict(X_test)
+
